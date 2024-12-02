785abcaf7337e021b6487eb1ed21ba68dbd497ef
zakaria-mourtaban <zakariamourtaban@gmail.com>
2024-12-02 11:13:49 +0200

    added projects model

---
+++ laravel/app/Models/Project.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+	
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+}
