9cc99714b58dd60e517a1582d5150a101eb64209
zakaria-mourtaban <zakariamourtaban@gmail.com>
2024-12-02 12:21:40 +0200

    added random color to context

--- react-app/src/context/projectsContext.jsx
+++ react-app/src/context/projectsContext.jsx
@@ -1,42 +1,54 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
-import apiRoutes from './apis'; // Import the apiRoutes object
+import React, { createContext, useContext, useState, useEffect } from "react";
+import apiRoutes from "../utils/api";
 
-// Create a context for the projects data
 const ProjectsContext = createContext();
 
-// Create a custom hook to use the projects context
 export const useProjects = () => {
-  return useContext(ProjectsContext);
+	return useContext(ProjectsContext);
 };
 
-// Create a provider component to wrap your app and provide the context value
+function getRandomColor() {
+	const colors = ["red", "blue", "green", "yellow", "orange", "purple"];
+	const randomColor = colors[Math.floor(Math.random() * colors.length)];
+	return randomColor;
+}
+
 export const ProjectsProvider = ({ children }) => {
-  const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+	const [projects, setProjects] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const response = await fetch(apiRoutes.getProjects);
-        if (!response.ok) {
-          throw new Error('Failed to fetch projects');
-        }
-        const data = await response.json();
-        setProjects(data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
+	useEffect(() => {
+		const fetchProjects = async () => {
+			try {
+				const response = await fetch(apiRoutes.getProjects);
+				if (!response.ok) {
+					throw new Error("Failed to fetch projects");
+				}
+				const data = await response.json();
 
-    fetchProjects();
-  }, []);
+				const projectsNew = data.map((project) => ({
+					...project,
+					members: project.members.map((member) => ({
+						...member,
+						color: getRandomColor(),
+					})),
+				}));
 
-  return (
-    <ProjectsContext.Provider value={{ projects, loading, error }}>
-      {children}
-    </ProjectsContext.Provider>
-  );
+				setProjects(projectsNew);
+			} catch (err) {
+				setError(err.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchProjects();
+	}, []);
+
+	return (
+		<ProjectsContext.Provider value={{ projects, loading, error }}>
+			{children}
+		</ProjectsContext.Provider>
+	);
 };
